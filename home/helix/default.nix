6fadbc076e7f2a8528058cc06d8ec7a9d0472305
motheki <motheki@icloud.com>
2024-06-07 11:48:07 -0500

    super duper helix

--- home/helix/default.nix
+++ home/helix/default.nix
@@ -12,6 +12,13 @@
       editor = {
         line-number = "relative";
         lsp.display-messages = true;
+        lsp.display-inlay-hints = true;
+        lsp.snippets = true;
+        cursor-shape.insert = "bar";
+        true-color = true;
+        bufferline = "multiple";
+        insert-final-newline = false;
+        popup-border = "all"
       };
     };
   };
