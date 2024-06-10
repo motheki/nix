5c98dc15d665e651722cee6a4856ff7999b5c647
motheki <motheki@icloud.com>
2024-06-09 23:53:12 -0500

    unfree and broken software

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -4,6 +4,10 @@
   nix.settings.experimental-features = "nix-command flakes";
   services.nix-daemon.enable = true;
   security.pam.enableSudoTouchIdAuth = true;
+  nixpkgs.config = {
+    allowBroken = true;
+    allowUnfree = true;
+  }
   nix.package = pkgs.nix;
   programs.zsh.enable = true;
   nixpkgs.hostPlatform = "aarch64-darwin";
