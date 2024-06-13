462d0ea54177fceb87caf30907318809e771e064
motheki <motheki@icloud.com>
2024-06-13 14:41:35 -0500

    dohb

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -1,6 +1,13 @@
-{inpts, nixkgs, ...}:
-
-{
-  programs.nixvim.enable = true;
-  
+{inputs, ...}: {
+  imports = [
+    inputs.nixvim.homeManagerModules.nixvim
+  ];
+  home.shellAliases.v = "nvim";
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    luaLoader.enable = true;
+  };
 }
