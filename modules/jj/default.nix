beb454280dab5b09c51c9b8c3f7a177f45ff2c85
Trevor Opiyo <trevoropiyo@trevoropiyo.com>
2025-10-30 23:22:18 -0500

    jujutsu

--- modules/jj/default.nix
+++ modules/jj/default.nix
@@ -1,6 +1,30 @@
 {...}: {
   programs.jujutsu = {
     enable = true;
+    settings = {
+      user = {
+        email = "trevoropiyo@trevoropiyo.com";
+        name = "Trevor Opiyo";
+      };
+      ui = {
+        pager = "delta";
+        diff-formatter = ":git";
+        editor = "hx";
+      };
+      signing = {
+        behavior = "own";
+        backend = "ssh";
+        key = "~/.ssh/trevoropiyo.pub";
+        backends = {
+          ssh = {
+            allowed-signers = "~/.ssh/allowed_signers";
+          };
+        };
+      };
+      git = {
+        sign-on-push = true;
+      };
+    };
   };
   programs.jjui = {
     enable = true;
